1e76335d17a93c4e657d537add772c7569d4e3ed
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-12-23 09:27:37 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.send(`<h1>Yes, I Am Ok</h1>`);
+});
+
+app.get("/dev", async (req, res) => {
+    res.send(`<h1>I Am Full-Stack Developer</h1>`);
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
